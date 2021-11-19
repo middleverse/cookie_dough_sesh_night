afb84a248fbb027a0008090edd04e0ca9d530438
middleverse <arshi_b@hotmail.com>
2021-11-18 19:56:41 -0500

    src: basic route and server code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello world");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Sessions Tut");
+});
+
+app.listen(5000, () => console.log("Server Running on http://localhost:5000"));
