c9f9e53232f13b145abdc356e1159af5a71357c7
middleverse <arshi_b@hotmail.com>
2021-11-18 20:09:06 -0500

    src: added session middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import session from "express-session";
 
 const app = express();
 
+// session middleware
+app.use(
+  session({
+    secret: "lets sign this cookie",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
 app.get("/", (req, res) => {
+  //@ts-ignore
   res.send("Hello Sessions Tut");
 });
 
