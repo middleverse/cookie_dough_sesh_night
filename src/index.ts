aa6025591ce255d818f0fc7ac66706ab3e51ca03
middleverse <arshi_b@hotmail.com>
2021-11-21 20:37:42 -0500

    src: connected to mongodb using mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,38 @@
 import express from "express";
 import session from "express-session";
+import mongoose from "mongoose";
+const { MongoClient } = require("mongodb");
+import { createSchema, Type, typedModel } from "ts-mongoose";
 
 const app = express();
 
+const uri =
+  "mongodb+srv://admin:7xw8NYdzDL2Y9HZrAcfJMyH6PSZLNnxY@friedpancake.wd9jp.mongodb.net/TEMPDB?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// });
+// client.connect((err: any) => {
+//   const collection = client.db("TEMPDB").collection("Sessions");
+//   // perform actions on the collection object
+//   console.log("sessions: ", collection);
+//   client.close();
+// });
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((res) => {
+    console.log("MongoDB Connected!");
+  });
+
+// const connection = mongoose.createConnection(uri);
+
+// console.log("model names", connection);
 // session middleware
 app.use(
   session({
